56f864d304e8ae222735ec1979907c9f05c413a2
Vasil <vasil.derilov@gmail.com>
2022-08-06 22:05:27 +0300

    Added the functions from the last lecture, friday

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -1,75 +1,202 @@
-export function ReplaceDomain(email) {
-  return email.replace(/(@[a-z]*\.com)/g, "@novarto.com");
+// export function ReplaceDomain(email) {
+//   return email.replace(/(@[a-z]*\.com)/g, "@novarto.com");
+// }
+
+// export function Civilize(question) {
+//   return question.at(-1) === "?"
+//     ? [...question, ", please"].join("")
+//     : question.at(-1) !== "."
+//     ? [...question, "."].join("")
+//     : question;
+// }
+
+// export function Capitalize(word) {
+//   let capitalised = [];
+//   capitalised.push(word[0].toUpperCase());
+
+//   for (let i = 1; i < word.length; i++) {
+//     const element = word[i];
+//     capitalised.push(element.toLowerCase());
+//   }
+
+//   return capitalised.join("");
+// }
+
+// export function ValidateString(str) {
+//   const modified = str.split("");
+
+//   const tmp = modified.shift();
+//   modified.unshift(modified.pop());
+//   modified.push(tmp);
+
+//   return str.length > 1 ? modified.join("") : "Invalid String";
+// }
+
+// export function Java(str) {
+//   return str.slice(0, 4) == "Java";
+// }
+
+// export function CheckDates(firstDate, secondDate) {
+//   firstDate = Date(firstDate);
+//   secondDate = Date(secondDate);
+
+//   return firstDate > secondDate ? firstDate : secondDate;
+// }
+
+// export function Bomb(str) {
+//   return str.toLowerCase().includes("bomb") ? "Duck!!!" : "No bomb, relax.";
+// }
+
+// export function CalcPasta(countOfPeople) {
+//   return 10 + Math.ceil(countOfPeople * 0.25) * 20;
+// }
+
+// export function CarPrice(price, year) {
+//   if (year < 3) {
+//     return price * 0.8;
+//   } else if (year < 10) {
+//     return price * 0.5;
+//   } else {
+//     return price * 0.3;
+//   }
+// }
+
+// export function FindTheLowerWord(str) {
+//   const word = [];
+//   str = str.split("");
+
+//   str.forEach((element) => {
+//     if (element == element.toLowerCase()) {
+//       word.push(element);
+//     }
+//   });
+
+//   return word.join("");
+// }
+
+export function EvaluateArray(arr) {
+  return arr.every((bool) => bool);
 }
 
-export function Civilize(question) {
-  return question.at(-1) === "?"
-    ? [...question, ", please"].join("")
-    : question.at(-1) !== "."
-    ? [...question, "."].join("")
-    : question;
+export function FindNemo(sentance) {
+  sentance = sentance.split(" ");
+
+  return sentance.findIndex((word) => word === "Nemo") + 1;
 }
 
-export function Capitalize(word) {
-  let capitalised = [];
-  capitalised.push(word[0].toUpperCase());
+export function SumNumbers(arr) {
+  let sum = 0;
 
-  for (let i = 1; i < word.length; i++) {
-    const element = word[i];
-    capitalised.push(element.toLowerCase());
-  }
-
-  return capitalised.join("");
-}
-
-export function ValidateString(str) {
-  const modified = str.split("");
-
-  const tmp = modified.shift();
-  modified.unshift(modified.pop());
-  modified.push(tmp);
-
-  return str.length > 1 ? modified.join("") : "Invalid String";
-}
-
-export function Java(str) {
-  return str.slice(0, 4) == "Java";
-}
-
-export function CheckDates(firstDate, secondDate) {
-  firstDate = Date(firstDate);
-  secondDate = Date(secondDate);
-
-  return firstDate > secondDate ? firstDate : secondDate;
-}
-
-export function Bomb(str) {
-  return str.toLowerCase().includes("bomb") ? "Duck!!!" : "No bomb, relax.";
-}
-
-export function CalcPasta(countOfPeople) {
-  return 10 + Math.ceil(countOfPeople * 0.25) * 20;
-}
-
-export function CarPrice(price, year) {
-  if (year < 3) {
-    return price * 0.8;
-  } else if (year < 10) {
-    return price * 0.5;
-  } else {
-    return price * 0.3;
-  }
-}
-
-export function FindTheLowerWord(str) {
-  const word = [];
-  str = str.split("");
-
-  str.forEach((element) => {
-    if (element == element.toLowerCase()) {
-      word.push(element);
-    }
+  arr.forEach((element) => {
+    if (typeof element === typeof 1) sum += element;
   });
 
-  return word.join("");
+  return sum;
+}
+
+export function CalcVolume(boxes) {
+  let totalVolume = 0;
+
+  boxes.forEach((box) => {
+    totalVolume += box[0] * box[1] * box[2];
+  });
+
+  return totalVolume;
+}
+
+export function IsArrayUnique(arr) {
+  let isUnique = true;
+
+  for (let i = 0; i < arr.length; i += 2) {
+    const n = arr[i];
+
+    if (n % 2 !== 0) {
+      isUnique = false;
+      break;
+    }
+  }
+
+  for (let i = 1; i < arr.length; i += 2) {
+    const n = arr[i];
+
+    if (n % 2 === 0) {
+      isUnique = false;
+      break;
+    }
+  }
+
+  return isUnique;
+}
+
+export function MirrorArray(arr) {
+  const mirror = arr.slice(0, arr.length - 1);
+  mirror.reverse();
+  return [...arr, ...mirror];
+}
+
+export function SortArray(arr, method) {
+  switch (method) {
+    case "ASC":
+      return arr.sort((a, b) => a - b);
+
+    case "DESC":
+      return arr.sort((a, b) => b - a);
+
+    default:
+      return arr;
+  }
+}
+
+export function RemoveDublicates(arr) {
+  return [...new Set(arr)];
+}
+
+export function IsFactorChain(arr) {
+  let isFactorChain = true;
+
+  arr.forEach((num, nextNumIndex) => {
+    if (arr[nextNumIndex] % num !== 0) isFactorChain = false;
+  });
+
+  return isFactorChain;
+}
+
+export function ProgressDays(arr) {
+  let progressDays = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) progressDays++;
+  }
+
+  return progressDays;
+}
+
+export function IsSpecialMatrix(arr) {
+  arr.forEach((str, i) => {
+    arr[i] = str.split(" ").map(Number);
+  });
+
+  const rowResults = [];
+  const colResults = [];
+
+  arr.forEach((row) => {
+    let total = 0;
+    row.forEach((n) => {
+      total += n;
+    });
+    rowResults.push(total);
+  });
+
+  for (let i = 0; i < arr[0].length; i++) {
+    let total = 0;
+    for (let j = 0; j < arr.length; j++) {
+      total += arr[i][j];
+    }
+    colResults.push(total);
+  }
+
+  return (
+    rowResults.every((val) => val === rowResults[0]) &&
+    colResults.every((val) => val === colResults[0])
+  );
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,74 +6,98 @@ import * as functions from "./functions";
 let logger = FakeConsole;
 
 const switchElement = document.getElementById("consoleSwitch");
-const switchDomainBtn = document.getElementById("1");
-const civilizeBtn = document.getElementById("3");
-const capitalizeBtn = document.getElementById("4");
-const validStringBtn = document.getElementById("5");
-const isItJavaBtn = document.getElementById("6");
-const checkDatesBtn = document.getElementById("7");
-const bombBtn = document.getElementById("8");
-const pastaBtn = document.getElementById("9");
-const bri4kaBtn = document.getElementById("10");
-const fugitiveWordBtn = document.getElementById("11");
-const sumAllPrecedingBtn = document.getElementById("12");
+const Btn1 = document.getElementById("1");
+const Btn2 = document.getElementById("2");
+const Btn3 = document.getElementById("3");
+const Btn4 = document.getElementById("4");
+const Btn5 = document.getElementById("5");
+const Btn6 = document.getElementById("6");
+const Btn7 = document.getElementById("7");
+const Btn8 = document.getElementById("8");
+const Btn9 = document.getElementById("9");
+const Btn10 = document.getElementById("10");
+const Btn11 = document.getElementById("11");
+const Btn12 = document.getElementById("12");
 
 Greet();
 SwitchConsole();
 
 switchElement.addEventListener("click", SwitchConsole);
 
-switchDomainBtn.addEventListener("click", (event) => {
-  const value = prompt("Enter email");
-  LogOutput(HandleInput(functions.ReplaceDomain, value), event);
+Btn1.addEventListener("click", (event) => {
+  let value = prompt("enter bools separated by spaces");
+  value = value.split(" ");
+
+  value.forEach((element, i) => {
+    if (element === "true") value[i] = true;
+    else if (element === "false") value[i] = false;
+  });
+
+  LogOutput(HandleInput(functions.EvaluateArray, value), event);
 });
 
-civilizeBtn.addEventListener("click", (event) => {
-  const value = prompt("Enter your question or sentance");
-  LogOutput(HandleInput(functions.Civilize, value), event);
+Btn2.addEventListener("click", (event) => {
+  const value = prompt("Enter Nemo sentance");
+  LogOutput(HandleInput(functions.FindNemo, value), event);
 });
 
-capitalizeBtn.addEventListener("click", (event) => {
-  const value = prompt("Enter string to capitalise");
-  LogOutput(HandleInput(functions.Capitalize, value), event);
+Btn3.addEventListener("click", (event) => {
+  const value = [1, 2, 3, true, false, "5", 2, [10]];
+  AlertUsedArray(value);
+  LogOutput(HandleInput(functions.SumNumbers, value), event);
 });
 
-validStringBtn.addEventListener("click", (event) => {
-  const value = prompt("Enter a valid string").trim();
-  LogOutput(HandleInput(functions.ValidateString, value), event);
+Btn4.addEventListener("click", (event) => {
+  const value = [
+    [1, 2, 1],
+    [2, 2, 2],
+    [3, 1, 2],
+  ];
+  AlertUsedArray(value);
+  LogOutput(HandleInput(functions.CalcVolume, value), event);
 });
 
-isItJavaBtn.addEventListener("click", (event) => {
-  const value = prompt("Enter word");
-  LogOutput(HandleInput(functions.Java, value), event);
+Btn5.addEventListener("click", (event) => {
+  const value = [2, 1, 4, 9, 6];
+  AlertUsedArray(value);
+  LogOutput(HandleInput(functions.IsArrayUnique, value), event);
 });
 
-checkDatesBtn.addEventListener("click", (event) => {
-  const value1 = prompt("Enter first date");
-  const value2 = prompt("Enter second date");
-  LogOutput(HandleInput(functions.CheckDates, value1, value2), event);
+Btn6.addEventListener("click", (event) => {
+  const value = [1, 2, 3, true, false, 5];
+  AlertUsedArray(value);
+  LogOutput(HandleInput(functions.MirrorArray, value), event);
 });
 
-bombBtn.addEventListener("click", (event) => {
-  const value = prompt("Enter jihad string");
-  LogOutput(HandleInput(functions.Bomb, value), event);
+Btn7.addEventListener("click", (event) => {
+  const value1 = [5, 8, 3, 8, 4, 7, 0, 6];
+  AlertUsedArray(value1);
+  const value2 = prompt("Enter sorting method");
+  LogOutput(HandleInput(functions.SortArray, value1, value2), event);
 });
 
-pastaBtn.addEventListener("click", (event) => {
-  const value = prompt("How many people will eat pasta?");
-  LogOutput(HandleInput(functions.CalcPasta, value), event);
+Btn8.addEventListener("click", (event) => {
+  const value = ["Dimitar", "Ivan", "Ivan", "Vasil", "Niki"];
+  AlertUsedArray(value);
+  LogOutput(HandleInput(functions.RemoveDublicates, value), event);
 });
 
-bri4kaBtn.addEventListener("click", (event) => {
-  const value1 = prompt("Enter car price");
-  const value2 = prompt("Enter car age");
-
-  LogOutput(HandleInput(functions.CarPrice, value1, value2), event);
+Btn9.addEventListener("click", (event) => {
+  const value = [3, 6, 12, 36];
+  AlertUsedArray(value);
+  LogOutput(HandleInput(functions.IsFactorChain, value), event);
 });
 
-fugitiveWordBtn.addEventListener("click", (event) => {
-  const value = prompt("Enter crowd of letters");
-  LogOutput(HandleInput(functions.FindTheLowerWord, value), event);
+Btn10.addEventListener("click", (event) => {
+  const value = [10, 11, 12, 9, 10];
+  AlertUsedArray(value);
+  LogOutput(HandleInput(functions.ProgressDays, value), event);
+});
+
+Btn11.addEventListener("click", (event) => {
+  const value = ["4 5 6", "6 5 4", "5 5 5"];
+  AlertUsedArray(value);
+  LogOutput(HandleInput(functions.IsSpecialMatrix, value), event);
 });
 
 function SwitchConsole() {
@@ -100,4 +124,10 @@ function HandleInput(callback) {
 
 function LogOutput(value, event) {
   logger.log(`The output of ${event.originalTarget.innerHTML} is : ${value}`);
+}
+
+function AlertUsedArray(arr) {
+  alert(
+    `The array that will be passed in is: ${arr}\nYou can change that in the source code`
+  );
 }
