62df5fd67e45aa77d59e716b9543f3843a579d58
Vasil <vasil.derilov@gmail.com>
2022-08-10 10:52:44 +0300

    Cleaned up the file for easy use in the future

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -24,81 +24,27 @@ SwitchConsole();
 
 switchElement.addEventListener("click", SwitchConsole);
 
-Btn1.addEventListener("click", (event) => {
-  let value = prompt("enter bools separated by spaces");
-  value = value.split(" ");
+Btn1.addEventListener("click", (event) => {});
 
-  value.forEach((element, i) => {
-    if (element === "true") value[i] = true;
-    else if (element === "false") value[i] = false;
-  });
+Btn2.addEventListener("click", (event) => {});
 
-  LogOutput(HandleInput(functions.EvaluateArray, value), event);
-});
+Btn3.addEventListener("click", (event) => {});
 
-Btn2.addEventListener("click", (event) => {
-  const value = prompt("Enter Nemo sentance");
-  LogOutput(HandleInput(functions.FindNemo, value), event);
-});
+Btn4.addEventListener("click", (event) => {});
 
-Btn3.addEventListener("click", (event) => {
-  const value = [1, 2, 3, true, false, "5", 2, [10]];
-  AlertUsedArray(value);
-  LogOutput(HandleInput(functions.SumNumbers, value), event);
-});
+Btn5.addEventListener("click", (event) => {});
 
-Btn4.addEventListener("click", (event) => {
-  const value = [
-    [1, 2, 1],
-    [2, 2, 2],
-    [3, 1, 2],
-  ];
-  AlertUsedArray(value);
-  LogOutput(HandleInput(functions.CalcVolume, value), event);
-});
+Btn6.addEventListener("click", (event) => {});
 
-Btn5.addEventListener("click", (event) => {
-  const value = [2, 1, 4, 9, 6];
-  AlertUsedArray(value);
-  LogOutput(HandleInput(functions.IsArrayUnique, value), event);
-});
+Btn7.addEventListener("click", (event) => {});
 
-Btn6.addEventListener("click", (event) => {
-  const value = [1, 2, 3, true, false, 5];
-  AlertUsedArray(value);
-  LogOutput(HandleInput(functions.MirrorArray, value), event);
-});
+Btn8.addEventListener("click", (event) => {});
 
-Btn7.addEventListener("click", (event) => {
-  const value1 = [5, 8, 3, 8, 4, 7, 0, 6];
-  AlertUsedArray(value1);
-  const value2 = prompt("Enter sorting method");
-  LogOutput(HandleInput(functions.SortArray, value1, value2), event);
-});
+Btn9.addEventListener("click", (event) => {});
 
-Btn8.addEventListener("click", (event) => {
-  const value = ["Dimitar", "Ivan", "Ivan", "Vasil", "Niki"];
-  AlertUsedArray(value);
-  LogOutput(HandleInput(functions.RemoveDublicates, value), event);
-});
+Btn10.addEventListener("click", (event) => {});
 
-Btn9.addEventListener("click", (event) => {
-  const value = [3, 6, 12, 36];
-  AlertUsedArray(value);
-  LogOutput(HandleInput(functions.IsFactorChain, value), event);
-});
-
-Btn10.addEventListener("click", (event) => {
-  const value = [10, 11, 12, 9, 10];
-  AlertUsedArray(value);
-  LogOutput(HandleInput(functions.ProgressDays, value), event);
-});
-
-Btn11.addEventListener("click", (event) => {
-  const value = ["4 5 6", "6 5 4", "5 5 5"];
-  AlertUsedArray(value);
-  LogOutput(HandleInput(functions.IsSpecialMatrix, value), event);
-});
+Btn11.addEventListener("click", (event) => {});
 
 function SwitchConsole() {
   if (switchElement.checked === true) {
